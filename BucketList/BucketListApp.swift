d58f9013b5a4723a7056434c756cb29672d1e364
cchiaramonte12 <82632889+cchiaramonte12@users.noreply.github.com>
2023-10-11 11:40:26 -0400

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Cameron Chiaramonte on 10/11/23.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
